a4949d75866ab6a51b3652545a9506cbe8aafa6c
Jeroen Pelgrims <git@jeroenpelgrims.com>
2025-02-06 18:37:24 +0100

    Add big data 2 files

---
+++ big-data-2/06_0_EXAMPLE_KafkaProducer.py
@@ -0,0 +1,35 @@
+import datetime
+import json
+import random
+import time
+
+from kafka import KafkaProducer
+
+broker_address = "localhost:9092"
+topic = "demo"
+
+
+class Event:
+    def __init__(self, event_time, word, count):
+        self.event_time = event_time
+        self.word = word
+        self.count = count
+
+
+def generate_event(event_time):
+    count = int(random.gauss(16, 4))
+    word = random.choice(["spark", "hadoop", "cassandra", "kafka", "python", "scala"])
+    return Event(event_time, word, count)
+
+
+producer = KafkaProducer(
+    bootstrap_servers=broker_address,
+    key_serializer=str.encode,
+    value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+)
+
+while True:
+    message = generate_event(str(datetime.datetime.now()))
+    producer.send(topic, key=str(message.word), value=message.__dict__)
+    producer.flush()
+    time.sleep(1)
